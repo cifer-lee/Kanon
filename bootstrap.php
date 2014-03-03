d56587ef07fd88151c23850723a14ad92f508ed3
Cifer <mantianyu@gmail.com>
2014-03-03 11:46:20 +0800

    采用绝对路径形式的 PHP 包含路径

--- bootstrap.php
+++ bootstrap.php
@@ -1,17 +1,34 @@
 <?php
 
 /**
+ * Set path macro
+ */
+if('/' === dirname(__FILE__)) {
+    define('ROOT', '');
+} else {
+    define('ROOT', dirname(__FILE__));
+}
+
+define('CONTROLLERS', ROOT . '/controllers');
+define('VIEWS', ROOT . '/views');
+define('MODELS', ROOT . '/models');
+define('DB', ROOT . '/models/sqlite');
+define('TEMPLATES', ROOT . '/templates');
+
+/**
  * Set include path
  */
 ini_set('include_path', ini_get('include_path') . PATH_SEPARATOR
-. './controllers' . PATH_SEPARATOR
-. './views' . PATH_SEPARATOR
-. './models' . PATH_SEPARATOR
-. './templates');
+. CONTROLLERS . PATH_SEPARATOR
+. VIEWS . PATH_SEPARATOR
+. MODELS . PATH_SEPARATOR
+. DB . PATH_SEPARATOR
+. TEMPLATES);
 
 require 'controller.php';
 require 'view.php';
 require 'model.php';
+require 'db.php';
 
 define ('HTTP_METHOD_GET', 'GET');
 define ('HTTP_METHOD_POST', 'POST');
