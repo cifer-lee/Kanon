7b4a85e53651e743de9280582b78e60cca94efc5
Cifer Lee <mantianyu@gmail.com>
2015-01-03 01:54:20 +0800

    Remove unused routes

--- routestrap.php
+++ routestrap.php
@@ -35,22 +35,3 @@ $router->add_route('/', 'GET', array(
     'controller' => 'index',
     'action' => 'index')
 );
-
-$router->add_route('/api/controllers', 'GET', array(
-    'controller' => 'panels',
-    'action' => 'get_panels')
-);
-
-$router->add_route('/api/controllers', 'POST', array(
-    'controller' => 'panels',
-    'action' => 'create_panel')
-);
-
-$router->add_route('/api/controllers/:controller_id', 'GET', 
-    array(
-        'controller' => 'panel',
-        'action' => 'panel_read'), 
-    array(
-        'controller_id' => '[0-9]+'
-    )
-);
