2ed6469246251312bfd21efeb11d2ff674830513
Cifer Lee <mantianyu@gmail.com>
2015-01-02 21:38:36 +0800

    Remove the unnessesary macro definition

--- bootstrap.php
+++ bootstrap.php
@@ -12,9 +12,6 @@ if('/' === dirname(__FILE__)) {
 define('CONTROLLERS', ROOT . '/controllers');
 define('VIEWS', ROOT . '/views');
 define('MODELS', ROOT . '/models');
-define('DB', ROOT . '/models/sqlite');
-define('SOCKET', ROOT . '/models/socket');
-define('UTILS', ROOT . '/models/utils');
 define('TEMPLATES', ROOT . '/templates');
 
 /**
@@ -24,13 +21,11 @@ ini_set('include_path', ini_get('include_path') . PATH_SEPARATOR
 . CONTROLLERS . PATH_SEPARATOR
 . VIEWS . PATH_SEPARATOR
 . MODELS . PATH_SEPARATOR
-. DB . PATH_SEPARATOR
 . TEMPLATES);
 
 require 'controller.php';
 require 'view.php';
 require 'model.php';
-require 'db.php';
 
 define ('HTTP_METHOD_GET', 'GET');
 define ('HTTP_METHOD_POST', 'POST');
